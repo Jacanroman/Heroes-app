b45a68c6908d3bff5e318302370a6a7a0321576f
Jacanroman <jacanroman@gmail.com>
2021-07-04 18:35:50 +0100

    Read argument URL

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,33 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router-dom';
+import {getHeroesById} from '../../selectors/getHeroesById';
 
 export const HeroScreen = () => {
+
+    //Sirve para extraer los parametros que van por la url
+    //const params  =  useParams();
+
+    //Y usamos la desestructuracion
+    const {heroeId} = useParams();
+    console.log(heroeId);
+
+    //Tercero con el heroeId llamamos la funcion getHeroId
+    const hero = getHeroesById(heroeId);
+
+    //si el argumento que pasamos por el url redirecionamos a "/"
+    if(!hero){
+        return <Redirect to="/" />
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero
+
+    console.log(hero)
     return (
         <div>
             <h1>HeroScreen</h1>
